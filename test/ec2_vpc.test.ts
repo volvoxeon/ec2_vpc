0be683ff9bab8b3463dfcc0177438829ae58f7ef
volvoxeon <yuhezhouxinyu@126.com>
2022-04-17 13:18:10 +0800

    init repo

---
+++ test/ec2_vpc.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Ec2Vpc from '../lib/ec2_vpc-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ec2_vpc-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Ec2Vpc.Ec2VpcStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
